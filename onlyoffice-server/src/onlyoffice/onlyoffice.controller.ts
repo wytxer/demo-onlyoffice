9cf6b8cf9dc0feb05abdd53d90e3b890b824733c
wytxer <wy@bszhct.com>
2024-02-02 15:59:08 +0800

    refactor：关闭 DTO 校验

--- onlyoffice-server/src/onlyoffice/onlyoffice.controller.ts
+++ onlyoffice-server/src/onlyoffice/onlyoffice.controller.ts
@@ -19,7 +19,8 @@ export class OnlyofficeController {
   // 这里表示成功的 statusCode 状态不能返回 201，否则会报错「这份文件无法保存。请检查连接设置或联系您的管理员」，因为在 Onlyoffice 如果 statusCode 不等于 200 认为是失败
   @HttpCode(HttpStatus.OK)
   async callback(
-    @Body() body: OnlyofficeCallbackDto,
+    // @Body() body: OnlyofficeCallbackDto,
+    @Body() body: any,
   ): Promise<OnlyofficeCallback> {
     return await this.onlyofficeService.callback(body);
   }
